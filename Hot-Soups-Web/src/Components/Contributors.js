ea48db3998418d0e68060eb30c3528e2e6369358
Drummer Dee <drummer.deja@gmail.com>
2022-09-01 19:35:18 -0400

    test

--- Hot-Soups-Web/src/Components/Contributors.js
+++ Hot-Soups-Web/src/Components/Contributors.js
@@ -4,7 +4,7 @@ import '../Styles/Contributors.css';
 const Contributors = () => {
   return (
 <>
-    <header> Hi </header>
+    <header> Hi how are you </header>
     <div className='con-body'>Contributors</div>
 </>
   )
