2d5f1828a9b8fa067cd35232cb29585cc24a8461
Jasmine Vo <jasminepvo.dev@gmail.com>
2022-09-08 15:48:18 -0400

    Add design outline to contributors page based on the figma design

--- Hot-Soups-Web/src/Components/Contributors.js
+++ Hot-Soups-Web/src/Components/Contributors.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import '../Styles/Contributors.css';
+import React from "react";
+import "../Styles/Contributors.css";
+import DiscordIcon from "../Assests/Icons/discord-fill.svg";
+import GithubIcon from "../Assests/Icons/github-fill.svg";
+import LinkedInIcon from "../Assests/Icons/linkedin-box-fill.svg";
 
 const Contributors = () => {
-  return (
-<>
-    <header> Hi how are you </header>
-    <div className='con-body'>Contributors</div>
-</>
-  )
-}
+	return (
+		<>
+			<header> Start of Contributors Page </header>
+			<div className="con-body">
+				<div className="contributors">
+					<h3>Contributors</h3>
+				</div>
+				<div className="contributors-frontend">
+					<h2>Front End</h2>
+					<div className="contributor">
+						<div className="con-image">
+							<img className="con-headshot" alt="headshot"></img>
+						</div>
+						<div className="con-content">
+							<h1 className="con-name">Danny Phantom</h1>
+							<h2 className="con-title">Co-Founder</h2>
+						</div>
+						<div className="con-socials">
+							<img src={LinkedInIcon} alt="discord icon"></img>
+							<img src={GithubIcon} alt="discord icon"></img>
+							<img src={DiscordIcon} alt="discord icon"></img>
+						</div>
+					</div>
+					<div className="contributor">
+						<div className="con-image">
+							<img className="con-headshot" alt="headshot"></img>
+						</div>
+						<div className="con-content">
+							<h1 className="con-name">Jasmine Love</h1>
+							<h2 className="con-title">CEO</h2>
+						</div>
+						<div className="con-socials">
+							<img src={LinkedInIcon} alt="discord icon"></img>
+							<img src={GithubIcon} alt="discord icon"></img>
+							<img src={DiscordIcon} alt="discord icon"></img>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
 export default Contributors;
